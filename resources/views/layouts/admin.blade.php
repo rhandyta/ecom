a3dfd8cd95faf73977cded2a15d14b5ab2112cb3
rhandyta <rhandytabriantama@gmail.com>
2022-09-09 15:57:32 +0700

    color

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -31,6 +31,12 @@
                 <div class="content-wrapper">
                     @yield('content')
                 </div>
+                <footer class="footer">
+                <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © <a href="https://www.bootstrapdash.com/" target="_blank">bootstrapdash.com </a>{{ date('Y') }}</span>
+                    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Only the best <a href="https://www.bootstrapdash.com/" target="_blank"> Bootstrap dashboard  </a> templates</span>
+                </div>
+                </footer>
             </div>
         </div>
     </div>
